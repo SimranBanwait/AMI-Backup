aefb2bb55128c47484908c5f299c10c7b4940097
Simran <98958500+SimranBanwait@users.noreply.github.com>
2024-07-22 08:48:01 +0530

    Update ami.ps1

--- ami.ps1
+++ ami.ps1
@@ -4,17 +4,18 @@
 # server3
 
 $servers = Get-Content -Path "servers.txt"
+$region = "us-east-1"  # Specify your desired region here
 
 foreach ($server in $servers) {
     # Describe the instance to get the instance ID
-    $instance = aws ec2 describe-instances --filters "Name=tag:Name,Values=$server" --query "Reservations[*].Instances[*].InstanceId" --output text
+    $instance = aws ec2 describe-instances --region $region --filters "Name=tag:Name,Values=$server" --query "Reservations[*].Instances[*].InstanceId" --output text
 
     if ($instance) {
         # Create an AMI backup
-        $ami_id = aws ec2 create-image --instance-id $instance --name "$server-backup-$(Get-Date -Format 'yyyy-MM-dd-HH-mm')" --no-reboot --output text
+        $ami_id = aws ec2 create-image --region $region --instance-id $instance --name "$server-backup-$(Get-Date -Format 'yyyy-MM-dd-HH-mm')" --no-reboot --output text
 
         if ($ami_id) {
-            Write-Output "AMI backup fo server: $server started successfully with AMI ID: $ami_id"
+            Write-Output "AMI backup for server: "$server" started successfully with AMI ID: $ami_id"
         } else {
             Write-Output "Failed to create AMI backup for $server"
         }
